0d671635c64f230c99491a2eb36bcedf113c424c
tpiros <2042718+tpiros@users.noreply.github.com>
2020-08-13 18:32:23 +0200

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,8 +3,41 @@
  *
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
-
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
 module.exports = {
-  /* Your site config here */
-  plugins: [],
+  siteMetadata: {
+    title: `SummerShop`,
+    description: `SummerShop - Your Favourite Online Store`,
+    author: `Tamas Piros`,
+    company: `Full Stack Training Ltd`,
+    companyWebsite: `https://fullstacktraining.com`
+  },
+  plugins: [
+    `gatsby-plugin-postcss`,
+    `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `content`,
+        path: `${__dirname}/src/content`
+      }
+    },
+    {
+      resolve: `gatsby-plugin-layout`,
+      options: {
+        component: require.resolve(`./src/components/layout`)
+      }
+    },
+    {
+      resolve: `gatsby-plugin-snipcart`,
+      options: {
+        apiKey: process.env.SNIPCART,
+        js: `https://cdn.snipcart.com/themes/v3.0.19/default/snipcart.js`,
+        styles: `https://cdn.snipcart.com/themes/v3.0.19/default/snipcart.css`,
+        autopop: true
+      }
+    }
+  ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,13 @@
-import React from "react"
+import React from 'react'
 
-export default function Home() {
-  return <div>Hello world!</div>
+const IndexPage = () => {
+  return (
+    <>
+      <h1 className="font-bold text-2xl">Welcome to our store <span role="img" aria-label="Summer Shop">🏖️</span></h1>
+      <p className="p-5">In the summertime when the weather is hot, you can stretch right up and touch the sky</p>
+      <p className="p-5">In our online store, you can buy the finest Summer products for you and your family.</p>
+    </>
+  )
 }
+
+export default IndexPage
