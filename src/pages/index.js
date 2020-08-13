a29f853558505970daa7272467a11b6ea0c99ed3
tpiros <2042718+tpiros@users.noreply.github.com>
2020-08-13 18:36:59 +0200

    updated welcome message

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import React from 'react'
 const IndexPage = () => {
   return (
     <>
-      <h1 className="font-bold text-2xl">Welcome to our store <span role="img" aria-label="Summer Shop">🏖️</span></h1>
+      <h1 className="font-bold text-2xl">Welcome to our store <span role="img" aria-label="Summer Shop">🏖️</span>. The best store ever! ;)</h1>
       <p className="p-5">In the summertime when the weather is hot, you can stretch right up and touch the sky</p>
       <p className="p-5">In our online store, you can buy the finest Summer products for you and your family.</p>
     </>
